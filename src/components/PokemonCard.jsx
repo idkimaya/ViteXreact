f30b6df7e1b900a95d11d3ac791a6ade865600a0
idkimaya <112517716+idkimaya@users.noreply.github.com>
2023-03-29 16:46:07 +0200

    components.2 added

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const pokemonList = [
+	{
+	  name: "bulbasaur",
+	  imgSrc:
+		"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+	  name: "mew",
+	},
+  ];
+
+function PokemonCard() {
+	const pokemon = pokemonList[0];
+	return (
+		<figure>
+			{pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name}/> : <p>???</p>}
+			<figcaption>{thepokemon.name}</figcaption>
+		</figure>
+	);
+}
+
+export default PokemonCard;
