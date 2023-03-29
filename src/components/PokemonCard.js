507d5b1dd2f23d750530d0070dc6c04aab49d42f
idkimaya <112517716+idkimaya@users.noreply.github.com>
2023-03-29 15:37:44 +0200

    Added PokemonCard component

---
+++ src/components/PokemonCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MyPokemon = () => {
+    return (
+      <div>
+         <figure>
+         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"alt=""/>
+         <figcaption>Who's that pokemon ?</figcaption>
+       </figure>
+      </div>
+    );
+  };
+
+  export default MyPokemon;
